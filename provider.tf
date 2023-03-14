d35651aa6bad92f35321b735b3cd1476f9d1f4f3
Salu Teslim <saluayomide@gmail.com>
2023-03-15 00:31:06 +0100

    move

--- provider.tf
+++ provider.tf
@@ -2,3 +2,17 @@ provider "aws" {
   region = var.region
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.tes-eks-cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.tes-eks-cluster.certificate_authority[0].data)
+    config_context_cluster = aws_eks_cluster.tes-eks-cluster.name
+  }
+}
+
+provider "kubernetes" {
+    host                   = aws_eks_cluster.tes-eks-cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.tes-eks-cluster.certificate_authority[0].data)
+    config_context_cluster = aws_eks_cluster.tes-eks-cluster.name
+  
+}
